4d2d9d6854f930817c78703393761eae700631b2
Edward Wright <ed.wright@live.co.uk>
2018-01-01 21:34:14 +0000

    updates

--- app/scripts/guid.js
+++ app/scripts/guid.js
@@ -1,30 +1,36 @@
 function check_guid() {
-    'use strict';
-    if (Cookies.get('GUID')) {
-        console.log('I have the cookie');
+    "use strict";
+    if (Cookies.get("GUID")) {
         return true;
     } else {
-        console.log('I am getting the cookie');
         return false;
     }
 }
 
 function get_guid(api_addr) {
-    'use strict';
-    $.get(api_addr + '/getGUID', function (data, status) {
+    "use strict";
+    $.get(api_addr + "/guid", function (data, status) {
         //console.log("Data: " + data + "\nStatus: " + status);
         console.log(data);
-        if(data.id){
-            //Cookies.set('GUID', data.id, {expires: 7, secure: true});
-            console.warn('cookie is now being set securely, make sure to change this');
-            Cookies.set('GUID', data.id, {expires: 7});
+        if(data.guid){
+            //Cookies.set("GUID", data.id, {expires: 7, secure: true});
+            console.warn("cookie is not being set securely, make sure to change this");
+            Cookies.set("GUID", data.guid, {expires: 7});
+
+            $.post(api_addr + "/guid", {guid: data.guid}, function (output, status) {
+                if(output.result){
+                    console.log(output);
+                } else {
+                    console.log(output);
+                }
+            });
         }
     });
 }
 
 function guid() {
-    'use strict';
-    var api_addr = 'http://localhost:3000';
+    "use strict";
+    var api_addr = "http://localhost:3000";
     if (!check_guid()) {
         get_guid(api_addr);
     }
