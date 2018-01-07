e4b3b0b297aece318565a043b8602c6747364fcd
Edward Wright <ed.wright@live.co.uk>
2018-01-07 23:15:48 +0000

    update

---
+++ scripts/guid.js
@@ -0,0 +1,39 @@
+function check_guid() {
+    'use strict';
+    if (Cookies.get('GUID')) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function get_guid(api_addr) {
+    'use strict';
+    $.get(api_addr + '/guid', function (data, status) {
+        //console.log("Data: " + data + "\nStatus: " + status);
+        console.log(data);
+        if(data.guid){
+            //Cookies.set("GUID", data.id, {expires: 7, secure: true});
+            console.warn('cookie is not being set securely, make sure to change this');
+            Cookies.set('GUID', data.guid, {expires: 7});
+
+            $.post(api_addr + '/guid', {guid: data.guid}, function (output, status) {
+                if(output.result){
+                    console.log(output);
+                } else {
+                    console.log(output);
+                }
+            });
+        }
+    });
+}
+
+function guid() {
+    'use strict';
+    var api_addr = 'http://localhost:3000';
+    if (!check_guid()) {
+        get_guid(api_addr);
+    }
+}
+
+guid();
