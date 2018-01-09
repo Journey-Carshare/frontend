e705fbdaa4021b78eb75f54edd37d9fb05bcb1cf
Edward Wright <ed.wright@live.co.uk>
2018-01-09 20:43:27 +0000

    updates

---
+++ app/scripts/nav.js
@@ -0,0 +1,11 @@
+$("#peek").hide();
+
+$("#peek-show").click(function(){
+    console.log("show");
+    $("#peek").show();
+});
+
+$("#peek-hide").click(function(){
+    console.log("hide");
+    $("#peek").hide();
+});
