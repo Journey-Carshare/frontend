87d953ee8c1fdaf317d238437e0f32d0897912c4
Edward Wright <ed.wright@live.co.uk>
2018-01-13 22:20:24 +0000

    Update sw.js

--- scripts/sw.js
+++ scripts/sw.js
@@ -13,21 +13,21 @@ self.addEventListener('fetch', function(evt) {
 function precache() {
   return caches.open(CACHE).then(function (cache) {
     return cache.addAll([
-        './styles/main.css',
-        './styles/font-awesome.min.css',
-        './scripts/main.js',
-        './scripts/vendor.js',
-        'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
-        'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',
-        './images/road-tall.svg',
-        './images/favicon@0,1x.png',
-        './images/csev1o.jpg',
-        './images/csev2o.jpg',
-        './images/csev3o.jpg',
-        './fonts/Roboto-Regular.ttf',
-        './fonts/Roboto-Bold.ttf',
-        './fonts/Roboto-Medium.ttf',
-        './index.html'
+        '../styles/main.css',
+        '../styles/font-awesome.min.css',
+        '../scripts/main.js',
+        '../scripts/vendor.js',
+        // 'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
+        // 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',
+        '../images/road-tall.svg',
+        '../images/favicon@0,1x.png',
+        '../images/csev1o.jpg',
+        '../images/csev2o.jpg',
+        '../images/csev3o.jpg',
+        '../fonts/Roboto-Regular.ttf',
+        '../fonts/Roboto-Bold.ttf',
+        '../fonts/Roboto-Medium.ttf',
+        '../index.html'
     ]);
   });
 }
@@ -46,4 +46,3 @@ function update(request) {
     });
   });
 }
-
