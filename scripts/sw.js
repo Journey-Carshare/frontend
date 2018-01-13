1e5c4d32649f5714b751d8ac9ea32c65d1e17f14
Edward Wright <ed.wright@live.co.uk>
2018-01-13 19:44:48 +0000

    updates- jan 2

--- scripts/sw.js
+++ scripts/sw.js
@@ -1,1 +1,25 @@
-'use strict'
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        [
+          '/styles/main.css',
+          '/styles/font-awesome.min.css',
+          '/scripts/main.js',
+          '/scripts/vendor.js',
+          'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js',
+          'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js',
+          'images/road-tall.svg',
+          'images/favicon@0,1x.png',
+          'images/csev1o.jpg',
+          'images/csev2o.jpg',
+          'images/csev3o.jpg',
+          'fonts/Roboto-Regular.ttf',
+          'fonts/Roboto-Bold.ttf',
+          'fonts/Roboto-Medium.ttf',
+          '/index.html'
+        ]
+      );
+    })
+  );
+});
