a43987010ab5e7aac2c2d7729195b73dbb66523f
Edward Wright <ed.wright@live.co.uk>
2018-01-23 23:13:45 +0000

    updates

--- app/scripts/calendar.js
+++ app/scripts/calendar.js
@@ -1,20 +1,20 @@
 function setCalendarMonthTitle(dateIn) {
-    "use strict";
+    'use strict';
     var today = dateIn;
-    var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
-    var output = months[today.getMonth()] + " " + today.getFullYear();
-    document.getElementById("titleMonth").innerHTML = output;
+    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    var output = months[today.getMonth()] + ' ' + today.getFullYear();
+    document.getElementById('titleMonth').innerHTML = output;
 }
 
 function setCalendarWeekTitle(calendar) {
-    "use strict";
-    var output = calendar.week[0].date + " - " + calendar.week[4].date;
-    document.getElementById("titleWeek").innerHTML = output;
+    'use strict';
+    var output = calendar.week[0].date + ' - ' + calendar.week[4].date;
+    document.getElementById('titleWeek').innerHTML = output;
 }
 
 function setCalendarArray(dateIn) {
-    "use strict";
-    var days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    'use strict';
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
     var today = dateIn;
     var calendar = {
         week: []
@@ -25,30 +25,72 @@ function setCalendarArray(dateIn) {
         var day1 = days[today.getDay() + i];
 
         calendar.week.push({
-            "day": day1,
-            "date": date1
+            'day': day1,
+            'date': date1
         });
     }
     return calendar;
 }
 
+// function setupCalendarDiv(calendar) {
+//     "use strict";
+//     var output = "";
+//     var j;
+//     $()
+//     for(j = 0; j < 5; j++){
+//         output += "<div class='mat-cal-foot-day-element'><div class='date'>";
+//         output += calendar.week[j].date;
+//         output += "<div class='day'>";
+//         output += calendar.week[j].day;
+//         output += "</div></div><div class='mat-cal-foot-day-journey'><div class='mat-cal-foot-day-journey-element bg-morn'><div class='title'>To Work</div><div class='location'><i class='fa fa-map-marker'></i> UB7 9HF</div><div class='time'><i class='fa fa-clock-o'></i> 07:40</div><div class='icon'><i class='fa fa-sun-o'></i></div></div><div class='mat-cal-foot-day-journey-element bg-aft'><div class='title'>To Home</div><div class='location'><i class='fa fa-map-marker'></i> RG2 6GF</div><div class='time'><i class='fa fa-clock-o'></i> 16:00</div><div class='icon'><i class='fa fa-moon-o'></i></div></div></div></div>";
+//     }
+//     document.getElementById("calendarContainer").innerHTML = output;
+// }
+
+function getCalendarJSON(){
+    return $.getJSON( 'data/calendar.json').then(function(data) {
+        return data;
+    });
+};
+
 function setupCalendarDiv(calendar) {
-    "use strict";
-    var output = "";
+    'use strict';
+    var output = '';
     var j;
-    for(j = 0; j < 5; j++){
-        output += "<div class='mat-cal-foot-day-element'><div class='date'>";
-        output += calendar.week[j].date;
-        output += "<div class='day'>";
-        output += calendar.week[j].day;
-        output += "</div></div><div class='mat-cal-foot-day-journey'><div class='mat-cal-foot-day-journey-element bg-morn'><div class='title'>To Work</div><div class='location'><i class='fa fa-map-marker'></i> UB7 9HF</div><div class='time'><i class='fa fa-clock-o'></i> 07:40</div><div class='icon'><i class='fa fa-sun-o'></i></div></div><div class='mat-cal-foot-day-journey-element bg-aft'><div class='title'>To Home</div><div class='location'><i class='fa fa-map-marker'></i> RG2 6GF</div><div class='time'><i class='fa fa-clock-o'></i> 16:00</div><div class='icon'><i class='fa fa-moon-o'></i></div></div></div></div>";
-        console.log(output);
-    }
-    document.getElementById("calendarContainer").innerHTML = output;
+    getCalendarJSON().then(function(returndata){
+        var calData = returndata;
+        for(j = 0; j < 5; j++){
+            var k;
+            output += '<div class=\'mat-cal-foot-day-element\'><div class=\'date\'>';
+            output += calendar.week[j].date;
+            output += '<div class=\'day\'>';
+            output += calendar.week[j].day;
+            output += '</div></div><div class=\'mat-cal-foot-day-journey\'>';
+            for(k = 0; k < calData.calendar[j].journeys.length; k++){
+                output += '<div class=\'mat-cal-foot-day-journey-element bg-' + calData.calendar[j].journeys[k].event + '\'><div class=\'title\'>';
+                output += calData.calendar[j].journeys[k].name;
+                output += '</div><div class=\'location\'><i class=\'fa fa-map-marker\'></i> ' ;
+                output += calData.calendar[j].journeys[k].postcode;
+                output += '</div><div class=\'time\'><i class=\'fa fa-clock-o\'></i> ';
+                output += calData.calendar[j].journeys[k].time;
+                var icon;
+                if(calData.calendar[j].journeys[k].event == 'danger'){
+                    icon = 'fa-minus-circle';
+                } else if(calData.calendar[j].journeys[k].event == 'evening'){
+                    icon = 'fa-moon-o';
+                } else {
+                    icon = 'fa-sun-o';
+                }
+                output += '</div><div class=\'icon\'><i class=\'fa ' + icon + '\'></i></div></div>';
+            }
+                output += '</div></div></div></div>';
+        }
+        document.getElementById('calendarContainer').innerHTML = output;
+    });
 }
 
 function setupCalendar() {
-    "use strict";
+    'use strict';
     var today = new Date();
     setCalendarMonthTitle(today);
     var calendar = setCalendarArray(today);
--- app/scripts/nav.js
+++ app/scripts/nav.js
@@ -1,11 +1,11 @@
 $('#peek').hide();
 
 $('#peek-show').click(function(){
-    console.log('show');
+    // console.log('show');
     $('#peek').show();
 });
 
 $('#peek-hide').click(function(){
-    console.log('hide');
+    // console.log('hide');
     $('#peek').hide();
 });
